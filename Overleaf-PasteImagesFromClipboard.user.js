a38571b5630ce5afd46360c1b0a324fe1a78dcd3
Sebastian Haas <73472903+cmprmsd@users.noreply.github.com>
2021-02-14 11:58:17 +0100

    Add user script, demo gif and edit README

---
+++ Overleaf-PasteImagesFromClipboard.user.js
@@ -0,0 +1,124 @@
+// ==UserScript==
+// @name         Overleaf - Paste Images from Clipboard
+// @namespace    http://sebastianhaas.de
+// @version      0.5
+// @description  Paste images from your clipboard directly into Overleaf (Community Edition, Cloud and Pro)
+// @author       Sebastian Haas
+// @match        https://www.overleaf.com/project/*
+// @match        http://192.168.100.239/project/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js
+// @grant        none
+// ==/UserScript==
+
+// Parse images from the clipboard
+function retrieveImageFromClipboardAsBlob(pasteEvent, callback){
+    if(pasteEvent.clipboardData == false){
+        if(typeof(callback) == "function"){
+            callback(undefined);
+        }
+    };
+
+    var items = pasteEvent.clipboardData.items;
+
+    if(items == undefined){
+        if(typeof(callback) == "function"){
+            callback(undefined);
+        }
+    };
+
+    for (var i = 0; i < items.length; i++) {
+        // Skip content if not image
+        if (items[i].type.indexOf("image") == -1) continue;
+        // Retrieve image on clipboard as blob
+        var blob = items[i].getAsFile();
+
+        if(typeof(callback) == "function"){
+            callback(blob);
+        }
+    }
+}
+
+// Upload the image blob
+function uploadImage(imageBlob,hash){
+    try{
+        var xhr = new XMLHttpRequest();
+        var url = document.location.pathname + "/upload?folder_id=" + _ide.fileTreeManager.findEntityByPath("assets").id + "&_csrf=" + csrfToken;
+        let formData = new FormData();
+        formData.append("qqfile", imageBlob, hash + ".png");
+        xhr.open("POST", url, true);
+        //xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var json = JSON.parse(xhr.responseText);
+                console.log(json.entity_id + " Asset created :)");
+            }
+        };
+        xhr.send(formData);
+    }catch(e)
+    {
+        console.log(e)
+    }
+};
+
+function checkAndCreateAssetsFolder(){
+    if (_ide.fileTreeManager.findEntityByPath("assets")){
+        console.log("Assets folder exists...")
+    }
+    else {
+        console.log("Assets folder does not exist...")
+        try {
+            _ide.fileTreeManager.createFolder("assets","/");
+        } catch(e) {
+            console.log(e);
+        }
+    }
+}
+
+// Editor Startup
+(function(e) {
+    try{
+        'use strict';
+        // poll until editor is loaded
+        const retry = setInterval(() => {
+            console.log("Polling...")
+            if (window._debug_editors === undefined) return
+            clearInterval(retry)
+            // get current editor instance
+            const editor = window._debug_editors[window._debug_editors.length -1]
+            // Create assets folder
+            checkAndCreateAssetsFolder();
+        }, 1000)
+        } catch(e) {
+            console.log(e);
+        }
+})();
+
+// Listen for paste events
+document.querySelector('.ace_editor').addEventListener('paste', function(e){
+    try {
+        // Handle the event
+        retrieveImageFromClipboardAsBlob(e, function(imageBlob){
+            // Image?
+            if(imageBlob){
+                checkAndCreateAssetsFolder();
+                var reader = new FileReader();
+                reader.readAsBinaryString(imageBlob);
+                reader.onloadend = function () {
+                    var  hash = CryptoJS.MD5(reader.result).toString().substring(0,8);
+                    console.log("Uploading image...")
+                    uploadImage(imageBlob,hash);
+                    _ide.editorManager.$scope.editor.sharejs_doc.ace.insert("\\begin{figure}[h!]\n\
+\t\\centering\n\
+\t\\includegraphics[width=0.66\\textwidth]{assets/" + hash + ".png}\n\
+\t\\caption{Caption}\n\
+\\end{figure}"
+                                                                           );
+                    _ide.editorManager.$scope.editor.sharejs_doc.ace.selection.moveCursorBy(-1,1);
+                    _ide.editorManager.$scope.editor.sharejs_doc.ace.selection.selectWordRight()
+                };
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }}
+                                                      );
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Overleaf-Image-Helper
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/donate?hosted_button_id=UPTPRDZGCRPJ8)
+
+## Description
+Overleaf currently does allow you to upload images only with their upload button.
+
+The purpose of this project is to provide a convenient way to insert images into Overleaf without menus by just using `ctrl+v`.  
+I wrote this as Tampermonkey script for Chrome/Chromium. The script will create an asset folder in your root folder and reference images in as LaTeX figures with captions, which you can directly edit after pasting the image.
+
+## Demo
+![demo](paste-from-clipboard.gif)
+
+## Installation
+
+1. Install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=de)
+2. Create new userscript or add from the raw user.js here from Github.
+3. Customize the appearance of the image on the last lines (line 110+) of the script.
+4. For community and self hosted pro editions modify the `@match` at the top of the script.
